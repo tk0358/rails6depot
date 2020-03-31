06be29044fb3f3584f076f0a29f0760c3b9d5d47
tk0358 <tk0358a@yahoo.co.jp>
2020-03-31 21:29:12 +0900

    error handling about record_not_found and add empty-cart button and cart show page with table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_061456) do
+ActiveRecord::Schema.define(version: 2020_03_31_065648) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
