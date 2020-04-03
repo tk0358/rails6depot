059ce05e08bddd326197bb6bfb4c353b3f00ddd6
tk0358 <tk0358a@yahoo.co.jp>
2020-04-03 21:48:37 +0900

    add check out function at chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :line_items
-  post 'line_items/:id/decrement', to: 'line_items#decrement', as: 'line_item_decrement'
+  resources :orders
+  resources :line_items do
+    post :decrement, on: :member
+  end
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
