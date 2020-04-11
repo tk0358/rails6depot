c6ca04f19ca45acea33983758e8f22a235389e49
tk0358 <tk0358a@yahoo.co.jp>
2020-04-12 01:08:24 +0900

    Change authorize to user respond_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,18 @@ class ApplicationController < ActionController::Base
   protected
 
     def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
+      respond_to do |format|
+        format.html {
+          unless User.find_by(id: session[:user_id])
+            redirect_to login_url, notice: "Please log in"
+          end
+        }
+        format.any {
+          authenticate_or_request_with_http_basic do |username, password|
+            user = User.find_by(name: username)
+            user && user.authenticate(password)
+          end
+        }
       end
     end
 end
